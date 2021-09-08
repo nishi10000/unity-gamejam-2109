2018b4a80bf380d7698ee0f935a2449b2d73e5db
yu <34505055+nishi10000@users.noreply.github.com>
2021-09-08 14:59:59 +0900

    水位の上昇のプロトタイプできた。

--- Assets/Scripts/PouringSystem/WaterLevelControl.cs
+++ Assets/Scripts/PouringSystem/WaterLevelControl.cs
@@ -14,27 +14,59 @@ public class WaterLevelControl : MonoBehaviour
     [SerializeField]
     CastingParameter CastingParameter = null;
 
-    //浮きを最初の位置に持ってくる。下から見たときに１以外になった初めての場所。//CastingParameter入力エンド後に実行する。
-    public void StartupPoisition()
-    {
-        //ターゲットの位置にUKIを置く。
-        UkiGameObject.transform.position = TargetObject.transform.position;
+    private float TagetObjectLowerPos = 0;  //鋳型底面
+    private float TagetObjectUpperPos = 0;　//鋳型上面
 
-        //ターゲットの位置の下にUkiを置く。//Rectのサイズを取得する。
-        float TagetObjectLowerPos = UkiGameObject.transform.position.y - TargetObject.GetComponent<SpriteRenderer>().bounds.size.y;
+    private int CastingLower = 0;  //鋳型の一番下を格納する。
+    private int CastingUpper = 0;  //鋳型の一番上を格納する。//要確認
+
+
+    //ターゲットのサイズを配列数で分割したものを格納する。1配列あたりの高さを算出。
+    private float ArrayPartHeight = 0;
+
+    //クリックしているかどうかを判断（仮）
+    public bool WaterLevelUpStart = false;
+
+    //水位が上がるベースの速度
+    [SerializeField]
+    private float LevelSpeed = 5.0f;
+
+    [SerializeField]
+    private float RateMagnification = 1.0f;  //形状に対してどれほど大げさに上昇率を変えるかを格納。
+
+
+    //鋳型形状確認後発火//CastingParameter入力エンド後に実行する。
+    public void Setup()
+    {
+        //鋳型の底面と上面を確認する。
+        WaterLevelPoisitionAnalys();
+        //鋳型の底面にUKIをもってくる。
+        SetStartupPoisition(TagetObjectLowerPos);
+    }
+
+
+    //鋳型の底面と上面を確認する
+    void WaterLevelPoisitionAnalys()
+    {
+
+        CastingLower = 0;  //初期化
+        CastingUpper = 0;  //初期化
+        //ターゲットの位置のRect下の場所を確認。//Rectのサイズを取得する。
+        TagetObjectLowerPos = TargetObject.transform.position.y - TargetObject.GetComponent<SpriteRenderer>().bounds.size.y;
         TagetObjectLowerPos = TagetObjectLowerPos / 2;
 
-        //ターゲットのサイズを配列数で分割する。
-        float ArrayPartHeight= TargetObject.GetComponent<SpriteRenderer>().bounds.size.y/ CastingParameter.CastingAlpha.Count;
+        TagetObjectUpperPos = TargetObject.transform.position.y + TargetObject.GetComponent<SpriteRenderer>().bounds.size.y;
+        TagetObjectUpperPos = TagetObjectUpperPos / 2;
 
-        int CastingLower = 0;  //鋳型の一番下を格納する。
-        int CastingUpper = 0;  //鋳型の一番上を格納する。//要確認
+        //ターゲットのサイズを配列数で分割する。1配列あたりの高さを算出。
+        ArrayPartHeight = TargetObject.GetComponent<SpriteRenderer>().bounds.size.y / CastingParameter.CastingAlpha.Count;
 
-        for (int i=0;i< CastingParameter.CastingAlpha.Count; i++)
+        
+        for (int i = 0; i < CastingParameter.CastingAlpha.Count; i++)
         {
-            if(Mathf.Approximately(CastingParameter.CastingAlpha[i], 1.0f))
+            if (Mathf.Approximately(CastingParameter.CastingAlpha[i], 1.0f))
             {
-                if (i < (CastingParameter.CastingAlpha.Count/2))  //TODO:この場合は半分より小さい図形に対応できない。
+                if (i < (CastingParameter.CastingAlpha.Count / 2))  //TODO:この場合は半分より小さい図形に対応できない。
                 {
                     CastingLower = i;
                 }
@@ -50,16 +82,53 @@ public class WaterLevelControl : MonoBehaviour
 
         //CastingLowerに合わせて高さを足した場所にUkiを設置。
         TagetObjectLowerPos = TagetObjectLowerPos + ArrayPartHeight * CastingLower;
+
+        //鋳型上面を算出
+        TagetObjectUpperPos = TagetObjectUpperPos - (ArrayPartHeight * (CastingParameter.CastingAlpha.Count - CastingUpper));
+    }
+
+    //浮きを最初の位置に持ってくる。下から見たときに１以外になった初めての場所。
+    public void SetStartupPoisition(float TagetObjectLowerPos)
+    {
+        //ターゲットの位置にUKIを置く。
+        UkiGameObject.transform.position = TargetObject.transform.position;
         UkiGameObject.transform.position = new Vector3(UkiGameObject.transform.position.x, TagetObjectLowerPos, UkiGameObject.transform.position.z);
     }
 
-
     private void Update()
     {
-        //goUp();
+        if (WaterLevelUpStart)
+        {
+            WaterLevelUp();
+        }
+        //途中で、Onbuttonが止まると,得点判定。//それは別クラスか？
     }
-    void goUp()
+
+    //スタートイベントを受けて動作開始。
+    //鋳型上面に向けて、Ukiを移動させる。
+    //Ukiが上面を超えたら、NGイベント発火。
+    void WaterLevelUp()
     {
-        UkiGameObject.transform.position += new Vector3(0 ,5.0f * Time.deltaTime, 0);
+        float Rate = 0;
+
+        //今のUkiの位置を確認する。
+        float nowHight = UkiGameObject.transform.position.y;
+
+        //初期値と今の高さを比べてどのぐらい上がったかを確認する。
+        float UpwardQuantity = nowHight - TagetObjectLowerPos;
+        
+        //上昇量に応じて配列の数字を上げる。
+        int ArrayForRaise =Mathf.FloorToInt(UpwardQuantity / ArrayPartHeight);
+
+        //配列に対して飛び越えないようにする。
+        if(CastingParameter.CastingAlpha.Count > (CastingLower + ArrayForRaise)) { 
+            Rate = CastingParameter.CastingAlpha[CastingLower + ArrayForRaise];
+        }
+        //TODO:思ったより速度の変化が分かりずらい。
+        Rate = RateMagnification * Rate;
+        //Debug.Log(Rate);
+
+        //その後TagetObjectUpperPosを超えると、NGイベント発火。
+        UkiGameObject.transform.position += new Vector3(0, LevelSpeed * Time.deltaTime * Rate, 0);
     }
 }
